4876183f70b4d8ff0d173077d306291143fdd19e
Paulo Vargas <paulotomegomesdevargas@hotmail.com>
2024-03-06 18:16:56 -0300

    Ajustes testes

--- paulovargas/tests/ProductList.test.tsx
+++ paulovargas/tests/ProductList.test.tsx
@@ -7,7 +7,11 @@ const mockProducts = getAllProducts();
 test('renders product list', () => {
   const { getAllByText } = render(<ProductList products={mockProducts} />);
   const productNames = mockProducts.map(product => product.name);
+  console.log('Produtos mock:', mockProducts);
+  console.log('Nomes dos produtos:', productNames);
   const regex = new RegExp(productNames.join('|'));
   const renderedProductNames = getAllByText(regex);
+  console.log('Número de produtos renderizados:', renderedProductNames.length);
+  console.log('Número esperado de produtos:', mockProducts.length);
   expect(renderedProductNames.length).toEqual(mockProducts.length);
 });
