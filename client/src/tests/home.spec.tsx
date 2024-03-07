d57e8ff7dcf10299d21190ae1c7df4cb3e4fae48
Pedro Henrique <63562843+pedrocorrea2002@users.noreply.github.com>
2024-03-07 14:17:22 -0300

    Update home.spec.tsx

--- client/src/tests/home.spec.tsx
+++ client/src/tests/home.spec.tsx
@@ -2,18 +2,17 @@ import {describe,expect,test} from "@jest/globals"
 import {render, screen} from "@testing-library/react"
 import Home from "@/app/page"
 
-describe("<Home/>", () => {
-    test("Testando se os produtos estão vindo corretamente",() => {
+describe("Testando se os produtos estão vindo corretamente"", () => {
+    test("testando se o número de produtos na tela é o mesmo da base de dados",() => {
         render(<Home/>)
-
-        .then(content => {
-            const produtos = screen.getAllByTestId("produto")
-
-            //testando se o número de produtos na tela é o mesmo da base de dados
-            expect(produtos.length).toBe(30)
-
-            //testando se o número de produtos na tela com o texto "pa" é o mesmo da base de dados
-            expect(produtos.filter(item => item.text.toLowerCase().match('pa')).length).toBe(3)
-        })
-    })
+        const produtos = screen.getAllByTestId("produto")
+        
+        expect(produtos.length).toBe(30)
+     })
+     test("testando se o número de produtos na tela com o texto 'pa' é o mesmo da base de dados", () => {
+        render(<Home/>)
+        const produtos = screen.getAllByTestId("produto")
+         
+        expect(produtos.filter(item => item.text.toLowerCase().match('pa')).length).toBe(3)
+     })
 })
